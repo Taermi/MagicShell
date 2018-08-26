e85ea4a2bf2358ee70fe0df3bef3c99273c7d469
Tærmi <35049928+Taermi@users.noreply.github.com>
2018-08-26 17:50:03 +0200

    More answers & Code improvement

--- src/com/github/taermi/Main.java
+++ src/com/github/taermi/Main.java
@@ -23,8 +23,9 @@ public class Main extends JavaPlugin implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		Player p = (Player) sender;
 		String prefix = ChatColor.DARK_GRAY + "-----[" + ChatColor.GOLD + "MagicShell" + ChatColor.DARK_GRAY + "]-----";
+		String spoken = ChatColor.GREEN + "And the" + ChatColor.GOLD + " Magic Shell " + ChatColor.GREEN + "has spoken: ";
 		if(cmd.getName().equalsIgnoreCase("ms")) {
-			int r = (int) Math.ceil(Math.random() * 3);
+			int r = (int) Math.ceil(Math.random() * 7);
 					
 			String message = "";
             for(int i = 0; i != args.length; i++)
@@ -33,13 +34,25 @@ public class Main extends JavaPlugin implements CommandExecutor {
             Bukkit.broadcastMessage(prefix);
             Bukkit.broadcastMessage(ChatColor.GOLD + p.getDisplayName() + ChatColor.GREEN + " asks: " + message);
             if(r == 1) {
-            	Bukkit.broadcastMessage(ChatColor.GREEN + "And the" + ChatColor.GOLD + " Magic Shell " + ChatColor.GREEN + "says: Yes!");
+            	Bukkit.broadcastMessage(spoken + "Yes.");
             }
             if(r == 2) {
-            	Bukkit.broadcastMessage(ChatColor.GREEN + "And the" + ChatColor.GOLD + " Magic Shell " + ChatColor.GREEN + "says: Maybe.");
+            	Bukkit.broadcastMessage(spoken + "Neither.");
             }
             if(r == 3) {
-            	Bukkit.broadcastMessage(ChatColor.GREEN + "And the" + ChatColor.GOLD + " Magic Shell " + ChatColor.GREEN + "says: No!");
+            	Bukkit.broadcastMessage(spoken + "No.");
+            }
+            if(r == 4) {
+            	Bukkit.broadcastMessage(spoken + "Maybe someday.");
+            }
+            if(r == 5) {
+            	Bukkit.broadcastMessage(spoken + "Try asking again.");
+            }
+            if(r == 6) {
+            	Bukkit.broadcastMessage(spoken + "Nothing.");
+            }
+            if(r == 7) {
+            	Bukkit.broadcastMessage(spoken + "I don't think so.");
             }
             
 			
